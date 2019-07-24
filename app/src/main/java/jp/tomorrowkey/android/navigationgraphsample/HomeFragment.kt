832d3f98bd54677e3c104343ed814e10aaffeda9
Tomoki Yamashita <tomorrowkey@gmail.com>
2019-07-24 15:33:50 +0900

    Add button and implement event handler

--- app/src/main/java/jp/tomorrowkey/android/navigationgraphsample/HomeFragment.kt
+++ app/src/main/java/jp/tomorrowkey/android/navigationgraphsample/HomeFragment.kt
@@ -1,6 +1,7 @@
 package jp.tomorrowkey.android.navigationgraphsample
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +9,22 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import jp.tomorrowkey.android.navigationgraphsample.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), HomeFragmentEventHandler {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
+        binding.handler = this
         return binding.root
     }
+
+    override fun onClickNextButton(view: View) {
+        Log.d("HomeFragment", "Hola!")
+    }
+}
+
+interface HomeFragmentEventHandler {
+    fun onClickNextButton(view: View)
 }
