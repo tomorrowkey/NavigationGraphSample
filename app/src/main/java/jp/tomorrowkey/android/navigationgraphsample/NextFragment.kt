bb0809e3ec1398b75226dea2464254ff6d3e5a40
Tomoki Yamashita <tomorrowkey@gmail.com>
2019-07-24 15:17:01 +0900

    Add NextFragment and connect by action from HomeAction

---
+++ app/src/main/java/jp/tomorrowkey/android/navigationgraphsample/NextFragment.kt
@@ -0,0 +1,17 @@
+package jp.tomorrowkey.android.navigationgraphsample
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NextFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_next, container, false)
+    }
+}
