5098742a0f464bcbd3c90e142f4cf70e97141b06
Tomoki Yamashita <tomorrowkey@gmail.com>
2019-07-24 16:12:30 +0900

    Implement pop button in NextFragment

--- app/src/main/java/jp/tomorrowkey/android/navigationgraphsample/NextFragment.kt
+++ app/src/main/java/jp/tomorrowkey/android/navigationgraphsample/NextFragment.kt
@@ -7,14 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import jp.tomorrowkey.android.navigationgraphsample.databinding.FragmentNextBinding
 
-class NextFragment : Fragment() {
+class NextFragment : Fragment(), NextFragmentHandler {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentNextBinding>(inflater, R.layout.fragment_next, container, false)
+        binding.handler = this
         return binding.root
     }
+
+    override fun onClickPopButton(view: View) {
+        view.findNavController().navigate(R.id.action_nextFragment_to_homeFragment)
+    }
+}
+
+interface NextFragmentHandler {
+    fun onClickPopButton(view: View)
 }
